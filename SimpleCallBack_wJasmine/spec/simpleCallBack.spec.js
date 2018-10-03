be172fd9e064ec0c99c53b27adac426b86af4ac9
FocusedBit <anthonyrayhollis@gmail.com>
2018-10-02 20:48:11 -0400

    added simpleCallBack with Jasmine

---
+++ SimpleCallBack_wJasmine/spec/simpleCallBack.spec.js
@@ -0,0 +1,5 @@
+describe("SimpleCallBack", function(){
+    it("says Badger and Blades", function(){
+        expect(simpleCallBack(getText)).toEqual("Badger and Blades");
+    });
+});
---
+++ SimpleCallBack_wJasmine/src/simpleCallBack.js
@@ -0,0 +1,9 @@
+var text = "Badger and Blades";
+
+function simpleCallBack(warriorCallBack) {
+    return warriorCallBack();
+}
+
+function getText(){
+    return text;
+}
